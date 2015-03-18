3995f17a512f07c02c0a2d1b12534a2d497f2a3e
zhangyun <zhangy95@163.com>
2015-03-18 23:58:16 +0800

    创建工程

---
+++ CloudImageCache/CloudImageCache.h
@@ -0,0 +1,13 @@
+//
+//  CloudImageCache.h
+//  CloudImageCache
+//
+//  Created by xunfeng on 15/3/18.
+//  Copyright (c) 2015年 darcyzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CloudImageCache : NSObject
+
+@end
---
+++ CloudImageCache/CloudImageCache.m
@@ -0,0 +1,13 @@
+//
+//  CloudImageCache.m
+//  CloudImageCache
+//
+//  Created by xunfeng on 15/3/18.
+//  Copyright (c) 2015年 darcyzhang. All rights reserved.
+//
+
+#import "CloudImageCache.h"
+
+@implementation CloudImageCache
+
+@end
